305f7876193a28e6957ed202615a2a6b48779cba
micstach <micstach@poczta.onet.pl>
2016-02-22 19:42:52 +0100

    project start

---
+++ app/src/main/java/com/example/micst/thelistclient/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.micst.thelistclient;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = (WebView) findViewById(R.id.webView) ;
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return true;
+            }
+        });
+        webView.getSettings().setUserAgentString("desktop client");
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("http://todo-micstach.rhcloud.com");
+    }
+}
